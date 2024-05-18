0eb6da8409290173be25daeacc4b96c37a4917d5
johannesalves <johannes001002@gmail.com>
2024-05-18 01:29:54 -0300

    initial commit

---
+++ app/help.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const Help = () => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default Help;
