f171eb76c8a6ecf2dd60a0e5fb44f77e66742b19
johannesalves <johannes001002@gmail.com>
2024-05-19 20:53:13 -0300

    chore: finished dropdown structure

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import RoundButton from './RoundButton';
 import * as DropdownMenu from 'zeego/dropdown-menu';
+import { Ionicons } from '@expo/vector-icons';
+import Colors from '@/constants/Colors';
 
 const Dropdown = () => {
   return (
@@ -9,6 +11,25 @@ const Dropdown = () => {
       <DropdownMenu.Trigger>
         <RoundButton icon={'ellipsis-horizontal'} text="More" />
       </DropdownMenu.Trigger>
+
+      <DropdownMenu.Content>
+        <DropdownMenu.Item key="statement">
+          <DropdownMenu.ItemTitle>Statement</DropdownMenu.ItemTitle>
+          <DropdownMenu.ItemIcon androidIconName="check_circle" />
+        </DropdownMenu.Item>
+
+        <DropdownMenu.Item key="converter">
+          <DropdownMenu.ItemTitle>Converter</DropdownMenu.ItemTitle>
+        </DropdownMenu.Item>
+
+        <DropdownMenu.Item key="account">
+          <DropdownMenu.ItemTitle>Add new account</DropdownMenu.ItemTitle>
+        </DropdownMenu.Item>
+
+        <DropdownMenu.Item key="backgound">
+          <DropdownMenu.ItemTitle>Background</DropdownMenu.ItemTitle>
+        </DropdownMenu.Item>
+      </DropdownMenu.Content>
     </DropdownMenu.Root>
   );
 };
