e47dce1ebf1ecd7d74b58e8b9f7297b29920d6cf
johannesalves <johannes001002@gmail.com>
2024-05-26 21:26:29 -0300

    feat: victoria chart

--- app/(authenticated)/crypto/[id].tsx
+++ app/(authenticated)/crypto/[id].tsx
@@ -1,26 +1,44 @@
 import { Stack, useLocalSearchParams } from 'expo-router';
-import React, { useState } from 'react';
 import {
-  StyleSheet,
   View,
   Text,
   SectionList,
+  StyleSheet,
   Image,
   TouchableOpacity,
   ScrollView,
+  TextInput,
 } from 'react-native';
 import { useHeaderHeight } from '@react-navigation/elements';
 import { defaultStyles } from '@/constants/Styles';
 import Colors from '@/constants/Colors';
 import { useQuery } from '@tanstack/react-query';
 import { Ionicons } from '@expo/vector-icons';
-
+import { useEffect, useState } from 'react';
+import { CartesianChart, Line, useChartPressState } from 'victory-native';
+import { Circle, useFont } from '@shopify/react-native-skia';
+import { format } from 'date-fns';
+import * as Haptics from 'expo-haptics';
+import Animated, { SharedValue, useAnimatedProps } from 'react-native-reanimated';
 const categories = ['Overview', 'News', 'Orders', 'Transactions'];
+
+Animated.addWhitelistedNativeProps({ text: true });
+const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
+
+function ToolTip({ x, y }: { x: SharedValue<number>; y: SharedValue<number> }) {
+  return <Circle cx={x} cy={y} r={8} color={Colors.primary} />;
+}
 
 const Page = () => {
   const { id } = useLocalSearchParams();
   const headerHeight = useHeaderHeight();
   const [activeIndex, setActiveIndex] = useState(0);
+  const font = useFont(require('@/assets/fonts/SpaceMono-Regular.ttf'), 12);
+  const { state, isActive } = useChartPressState({ x: 0, y: { price: 0 } });
+
+  useEffect(() => {
+    if (isActive) Haptics.selectionAsync();
+  }, [isActive]);
 
   const { data } = useQuery({
     queryKey: ['info', id],
@@ -34,6 +52,21 @@ const Page = () => {
   const { data: tickers } = useQuery({
     queryKey: ['tickers'],
     queryFn: async () => fetch(`/api/tickers`).then((res) => res.json()),
+  });
+
+  const animatedText = useAnimatedProps(() => {
+    return {
+      text: `${state.y.price.value.value.toFixed(2)} €`,
+      defaultValue: '',
+    };
+  });
+
+  const animatedDateText = useAnimatedProps(() => {
+    const date = new Date(state.x.value.value);
+    return {
+      text: `${date.toLocaleDateString()}`,
+      defaultValue: '',
+    };
   });
 
   return (
@@ -111,11 +144,66 @@ const Page = () => {
         )}
         renderItem={({ item }) => (
           <>
-            <View style={{ height: 500, backgroundColor: 'red' }}></View>
-
-            <View style={defaultStyles.block}>
-              <Text style={[styles.subtitle, { color: Colors.dark }]}>Overview</Text>
-              <Text style={{ color: Colors.gray }}>{data?.description}</Text>
+            <View style={[defaultStyles.block, { height: 500 }]}>
+              {tickers && (
+                <>
+                  {!isActive && (
+                    <View>
+                      <Text style={{ fontSize: 30, fontWeight: 'bold', color: Colors.dark }}>
+                        {tickers[tickers.length - 1].price.toFixed(2)} €
+                      </Text>
+                      <Text style={{ fontSize: 18, color: Colors.gray }}>Today</Text>
+                    </View>
+                  )}
+                  {isActive && (
+                    <View>
+                      <AnimatedTextInput
+                        editable={false}
+                        underlineColorAndroid={'transparent'}
+                        style={{ fontSize: 30, fontWeight: 'bold', color: Colors.dark }}
+                        animatedProps={animatedText}
+                      ></AnimatedTextInput>
+                      <AnimatedTextInput
+                        editable={false}
+                        underlineColorAndroid={'transparent'}
+                        style={{ fontSize: 18, color: Colors.gray }}
+                        animatedProps={animatedDateText}
+                      ></AnimatedTextInput>
+                    </View>
+                  )}
+                  <CartesianChart
+                    chartPressState={state}
+                    axisOptions={{
+                      font,
+                      tickCount: 5,
+                      labelOffset: { x: -2, y: 0 },
+                      labelColor: Colors.gray,
+                      formatYLabel: (v) => `${v} €`,
+                      formatXLabel: (ms) => format(new Date(ms), 'MM/yy'),
+                    }}
+                    data={tickers!}
+                    xKey="timestamp"
+                    yKeys={['price']}
+                  >
+                    {({ points }: any) => (
+                      <>
+                        <Line points={points.price} color={Colors.primary} strokeWidth={3} />
+                        {isActive && <ToolTip x={state.x.position} y={state.y.price.position} />}
+                      </>
+                    )}
+                  </CartesianChart>
+                </>
+              )}
+            </View>
+            <View style={[defaultStyles.block, { marginTop: 20 }]}>
+              <Text style={styles.subtitle}>Overview</Text>
+              <Text style={{ color: Colors.gray }}>
+                Bitcoin is a decentralized digital currency, without a central bank or single
+                administrator, that can be sent from user to user on the peer-to-peer bitcoin
+                network without the need for intermediaries. Transactions are verified by network
+                nodes through cryptography and recorded in a public distributed ledger called a
+                blockchain.
+              </Text>
             </View>
           </>
         )}
